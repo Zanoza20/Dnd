fb3dbdc2d4c0e5e78a864b2fc5b18ade35be144b
Danileyko <119929429+Zanoza20@users.noreply.github.com>
2025-02-18 11:02:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,8 +9,24 @@ function showAdventures() {
   document.getElementById('adventures-buttons').style.display = 'block';
 }
 
+function showCharacters() {
+  document.getElementById('details-buttons').style.display = 'none';
+  document.getElementById('characters-buttons').style.display = 'block';
+}
+
+function showNPC() {
+  document.getElementById('characters-buttons').style.display = 'none';
+  document.getElementById('npc-buttons').style.display = 'block';
+}
+
 function goBack() {
-  if (document.getElementById('adventures-buttons').style.display === 'block') {
+  if (document.getElementById('npc-buttons').style.display === 'block') {
+    document.getElementById('npc-buttons').style.display = 'none';
+    document.getElementById('characters-buttons').style.display = 'block';
+  } else if (document.getElementById('characters-buttons').style.display === 'block') {
+    document.getElementById('characters-buttons').style.display = 'none';
+    document.getElementById('details-buttons').style.display = 'block';
+  } else if (document.getElementById('adventures-buttons').style.display === 'block') {
     document.getElementById('adventures-buttons').style.display = 'none';
     document.getElementById('details-buttons').style.display = 'block';
   } else {
