7a213045d4a879388731e4ca9d44834aea4937af
Danileyko <119929429+Zanoza20@users.noreply.github.com>
2025-02-18 10:32:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,21 @@ function showDetails(section) {
   document.getElementById('back-button').style.display = 'block';
 }
 
-function goBack() {
+function showAdventures() {
   document.getElementById('details-buttons').style.display = 'none';
-  document.getElementById('initial-buttons').style.display = 'block';
-  document.getElementById('back-button').style.display = 'none';
+  document.getElementById('adventures-buttons').style.display = 'block';
+}
+
+function goBack() {
+  if (document.getElementById('adventures-buttons').style.display === 'block') {
+    document.getElementById('adventures-buttons').style.display = 'none';
+    document.getElementById('details-buttons').style.display = 'block';
+  } else {
+    document.getElementById('details-buttons').style.display = 'none';
+    document.getElementById('initial-buttons').style.display = 'block';
+    document.getElementById('back-button').style.display = 'none';
+  }
 }
 
 // Назначаем обработчик события для кнопки "Назад"
 document.getElementById('back-button').addEventListener('click', goBack);
- 
